101c24bf2275a3cee9534c908aa4d964cc7587d2
Karl Madl <karl_madl@yahoo.com>
2022-11-21 10:09:28 -0500

    added build_url_params method to Query class, refactored some naming in .yaml files.

--- nbacomquery/query.py
+++ nbacomquery/query.py
@@ -14,45 +14,43 @@ class Query:
     def __init__(self, stats_type: str, **kwargs) -> None:
         self.stats_type = stats_type
         self.tags = kwargs
-
         yaml = load_yaml(build_yaml_path(f"{stats_type}.yaml"))
-        self.headers, self.tag_dict = yaml["headers"], yaml["tags"]
-        self.validate_tags()
-        self.set_tags()
+        self.headers, self.param_dict = yaml["headers"], yaml["params"]
+        self.validate_params()
+        self.set_params()
+        self.url = f"{yaml['url']}{self.build_url_params()}"
 
-        self.url = f"https://stats.nba.com/stats/{yaml['url']}?" + "&".join(
-            [f"{key}={value}" for key, value in self.tag_dict.items()]
-        )
-
-    def validate_tags(self) -> None:
+    def validate_params(self) -> None:
         """
         Checks that all kwargs at class instantiation are valid query
-        parameters/tags.
+        parameters.
         """
         for tag in self.tags:
-            if tag not in self.tag_dict:
+            if tag not in self.param_dict:
                 raise ValueError(
                     f"{tag} is not a valid tag for {self.stats_type} stats"
                 )
 
-    def set_tags(self) -> None:
+    def set_params(self) -> None:
         """
-        Replaces all default query parameters that were specified by
+        Replaces any default query parameters that were specified by
         kwargs.
         """
         for tag in self.tags:
-            self.tag_dict[tag] = f"{self.tags[tag]}"
+            self.param_dict[tag] = f"{self.tags[tag]}"
+
+    def build_url_params(self) -> str:
+        """Returns query parameters formatted for url"""
+        return "&".join(
+            [f"{key}={value}" for key, value in self.param_dict.items()]
+        )
 
     def json(self) -> dict:
-        """
-        Returns raw JSON response from query.
-        """
+        """Returns raw JSON response from query."""
         return requests.get(self.url, headers=self.headers).json()
 
     def dataframe(self):
-        """
-        Parses JSON reponse and returns pandas DataFrame.
-        """
+        """Parses JSON reponse and returns pandas DataFrame."""
         response = self.json()["resultSets"][0]
         rows = response["rowSet"]
         col_names = response["headers"]
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.11"
+VERSION = "0.0.12"
 DESCRIPTION = "nba.com/stats querying package"
 LONG_DESCRIPTION = "nba.com/stats querying package and more information"
 
